8f1f01e457b385043abf3037ad9daaadcf22e4e7
Ali Artach <aliartach96@gmail.com>
2023-09-01 16:03:33 +0300

    commited-step1

---
+++ javascript-basics-02/step 1/step_1.js
@@ -0,0 +1,5 @@
+var targetedimage = document.querySelector("#image1");
+targetedimage.addEventListener("mouseover", function(){
+    targetedimage.src = "/javascript-basics-02/step 1/images/image1_2.jpg"
+});
+
