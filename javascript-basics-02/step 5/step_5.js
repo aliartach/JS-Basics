c6c3ce1ec7c65704ec4c6fb19a469218dbdc8a3f
Ali Artach <aliartach96@gmail.com>
2023-09-01 21:36:35 +0300

    completed-step5

---
+++ javascript-basics-02/step 5/step_5.js
@@ -0,0 +1,26 @@
+let images = [
+    document.querySelector("#image1"),
+    document.querySelector("#image2"),
+    document.querySelector("#image3"),
+    document.querySelector("#image4"),
+    document.querySelector("#image5")
+];
+
+function changeImage(event) {
+    let imageElement = event.target;
+    let imageName = imageElement.id;
+    
+    imageElement.src = `/javascript-basics-02/step 5/images/${imageName}_2.jpg`;
+}
+
+function restoreImage(event) {
+    let imageElement = event.target;
+    let imageName = imageElement.id;
+
+    imageElement.src = `/javascript-basics-02/step 5/images/${imageName}.jpg`;
+}
+
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener("mouseover", changeImage);
+    images[i].addEventListener("mouseout", restoreImage);
+}
